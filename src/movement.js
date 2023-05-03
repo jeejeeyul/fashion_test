bf0f1076c21a31965c48eae8f7adfb0bc768107f
jeejeeyul <jee200713@gmail.com>
2023-05-03 15:54:47 +0900

    갈아 엎고 이상형 월드컵처럼 만들기로 함/2023/05/23

--- src/movement.js
+++ src/movement.js
@@ -1,77 +1,26 @@
-import { questions } from './questions.js';
-import show_result from './result.js';
-var choose1 = 0;
-var choose2 = 0;
-var choose3 = 0;
-var choose4 = 0;
+var sets = new Array();
+var returnCount = 16; //선택지 개수
+
+function questionCount(i) {
+  for (let j = 0; j < i; j++) {
+    sets.push(j + 1);
+  }
+}
+
+var randomNumber = () => {
+  return Math.floor(Math.random() * sets.length);
+};
+
+questionCount(returnCount);
+console.log(sets);
+console.log(randomNumber());
 var ch1 = document.getElementById('ch1');
 var ch2 = document.getElementById('ch2');
-var i = 0;
-
-var descriptions_1 = questions.map((a) => {
-  return a[0];
-}); //1번 질문
-
-var descriptions_2 = questions.map((a) => {
-  return a[1];
-}); //2번 질문
-
-var amounts = questions.map((a) => {
-  return a[2];
-}); //더해지는 요소
-
-var amounts2 = questions.map((a) => {
-  return a[3];
-}); //더해지는 요소2
-
+var img1 = document.getElementById('img1');
+var img2 = document.getElementById('img2');
 ch1.addEventListener('click', function () {
-  if (amounts[i - 1] == 'choose1') {
-    choose1 = choose1 + 1;
-    console.log(choose1);
-  } else if (amounts[i - 1] == 'choose3') {
-    choose3 = choose3 + 1;
-    console.log(choose3);
-  } //가중치 더하기
-  ch1.textContent = '';
-  ch2.textContent = '';
-  var p1 = document.createElement('p');
-  p1.append(descriptions_1[i]);
-  ch1.append(p1);
-  var p2 = document.createElement('p');
-  p2.append(descriptions_2[i]);
-  ch2.append(p2); //다음 질문 표시
-  if (i == questions.length) {
-    ch1.textContent = '';
-    ch2.textContent = '';
-    ch1.style.visibility = 'hidden';
-    ch2.style.visibility = 'hidden';
-    show_result();
-  }
-  i++;
+  img1.setAttribute('src', `./images/${sets.pop(randomNumber())}.jpg`);
 });
-
 ch2.addEventListener('click', function () {
-  if (amounts2[i - 1] == 'choose2') {
-    choose2 = choose2 + 1;
-    console.log(choose2);
-  } else if (amounts2[i - 1] == 'choose4') {
-    choose4 = choose4 + 1;
-    console.log(choose4);
-  } //가중치 빼기
-  ch1.textContent = '';
-  ch2.textContent = '';
-  var p1 = document.createElement('p');
-  p1.append(descriptions_1[i]);
-  ch1.append(p1);
-  var p2 = document.createElement('p');
-  p2.append(descriptions_2[i]);
-  ch2.append(p2);
-  if (i == questions.length) {
-    ch1.textContent = '';
-    ch2.textContent = '';
-    ch1.style.visibility = 'hidden';
-    ch2.style.visibility = 'hidden';
-    show_result();
-  }
-  i++;
+  img2.setAttribute('src', `./images/${sets.pop(randomNumber())}.jpg`);
 });
--- src/result.js
+++
@@ -1,4 +0,0 @@
-export default function show_result() {
-  var result_btn = document.getElementById('result_btn');
-  result_btn.style.visibility = 'visible';
-}
